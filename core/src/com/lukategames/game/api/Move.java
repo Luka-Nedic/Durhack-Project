0cf06dfa2ada1e45c9e6a9e433e0521537757bad
Luka <luka.nedic@durham.ac.uk>
2019-11-23 18:09:32 +0000

    Kate: Adding API Features :)

---
+++ core/src/com/lukategames/game/api/Move.java
@@ -0,0 +1,51 @@
+package com.lukategames.game.api;
+
+public class Move {
+
+    private float initialXPosition;
+    private float finalXPosition;
+    private float initialYPosition;
+    private float finalYPosition;
+    private float moveSpeed;
+    private float currentXposition;
+    private float currentYposition;
+    private float gradient;
+    private boolean moving;
+
+    public void update() {
+        if(moving) {
+            if(initialYPosition != finalYPosition)
+                currentYposition += moveSpeed * gradient;
+            if(initialXPosition != finalXPosition)
+                currentXposition += moveSpeed * gradient;
+
+            if(finalXPosition == currentXposition && finalYPosition == currentYposition) {
+                moving = false;
+            }
+        }
+    }
+
+    public void move(float initialXPosition, float initialYPosition, float finalXPosition, float finalYPosition, float moveSpeed) {
+        this.initialXPosition = initialXPosition;
+        this.initialYPosition = initialYPosition;
+        this.finalXPosition = finalXPosition;
+        this.finalYPosition = finalYPosition;
+        this.currentYposition = initialYPosition;
+        this.currentXposition = initialYPosition;
+        this.moveSpeed = moveSpeed;
+        moving = true;
+        if(initialYPosition != finalYPosition && initialXPosition != initialYPosition) {
+            this.gradient = (finalYPosition - initialYPosition)/(finalXPosition - initialXPosition);
+        } else {
+            this.gradient = 1;
+        }
+    }
+
+    public float getXPosition() {
+        return currentXposition;
+    }
+    public float getYPosition() {
+        return currentYposition;
+    }
+
+}
---
+++ core/src/com/lukategames/game/api/Shake.java
@@ -0,0 +1,55 @@
+package com.lukategames.game.api;
+
+public class Shake {
+
+    private float initialPosition;
+    private float currentPosition;
+    private float shakeLength;
+    private float shakeSpeed;
+    private int counter;
+    private int cmove;
+    private int move;
+    private float shakeAmount;
+    private boolean shaking;
+
+    public void update() {
+        if(shaking) {
+
+            if (counter % 2 == 1) {
+                currentPosition += -shakeSpeed;
+            }
+            else {
+                currentPosition += shakeSpeed;
+            }
+            cmove += 1;
+
+            if(cmove >= move) {
+             cmove = 0;
+             counter += 1;
+            }
+
+            if(counter >= shakeAmount) {
+                currentPosition = initialPosition;
+                shaking = false;
+            }
+        }
+    }
+
+    public void shake(float initialPosition, float shakeLength, float shakeSpeed, float shakeAmount) {
+        this.currentPosition = initialPosition;
+        this.initialPosition = initialPosition;
+        this.shakeLength = shakeLength;
+        this.shakeSpeed = shakeSpeed;
+        this.counter = 0;
+        this.shakeAmount = shakeAmount;
+        this.cmove = 0;
+        this.move = Math.round(shakeLength/shakeSpeed);
+        shaking = true;
+        System.out.println(move);
+    }
+
+    public float getPosition() {
+        return currentPosition;
+    }
+
+}
